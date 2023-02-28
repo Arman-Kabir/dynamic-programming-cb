e1fb348a319e242eaaec3f246fbfefba68c4cb5d
Arman-Kabir <armankabir8@gmail.com>
2023-02-28 09:20:15 +0600

    grid traveler doing

---
+++ 2-38m.js
@@ -0,0 +1,10 @@
+
+// 2-38m // Grid Traveller
+
+/*
+    **Q.->  Say that, u r a traveler on a 2D grid. u begin in the top-left corner and your goal is to travel
+     to the bottom-right corner. u may only move down or right.
+
+     * In how many ways can you travel to the goal on a grid with dimensions m * n?
+     * Write a function `gridTraveler(m,n)` that calculates this.
+*/

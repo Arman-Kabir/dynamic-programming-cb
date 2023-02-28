4f7522ca7b486e4b5f6430718fb4dae434328afa
Arman-Kabir <armankabir8@gmail.com>
2023-02-28 10:01:28 +0600

    memoized grid traveler done

--- 2-38m.js
+++ 2-38m.js
@@ -9,11 +9,42 @@
      * Write a function `gridTraveler(m,n)` that calculates this.
 */
 
+/* 
 const gridTraveler = (m, n) => {
     if (m === 1 && n === 1) return 1;
     if (m === 0 || n === 0) return 0;
     return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
 };
+
+console.log(gridTraveler(1, 1));
+console.log(gridTraveler(2, 3));
+console.log(gridTraveler(3, 2));
+console.log(gridTraveler(3, 3));
+// console.log(gridTraveler(18, 18));
+
+// time com - O(2^n+m)  -- space com -- O(n+m)
+// now, let's improve the time
+*/
+
+
+// memoized code
+// memo obj in the parameter
+const gridTraveler = (m, n, memo = {}) => {
+    const key = m + ',' + n;
+    // memo checking obj -- are the args in the memo
+    if (key in memo) return memo[key];
+
+    if (m === 1 && n === 1) return 1;
+    if (m === 0 || n === 0) return 0;
+    memo[key] = gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+    return memo[key];
+};
+
+// m=42 
+// n = 3
+
+// key = 4,23
+
 
 console.log(gridTraveler(1, 1));
 console.log(gridTraveler(2, 3));

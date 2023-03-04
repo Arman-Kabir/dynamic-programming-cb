292ab575e2a21970c6df64ab6cca10290919686f
Arman-Kabir <armankabir8@gmail.com>
2023-03-04 11:51:42 +0600

    how sum doing

---
+++ 4-1h30m-howSum.js
@@ -0,0 +1,9 @@
+// Write a function `howSum(targetSum,numbers)` that takes in a targetSum and an array of numbers
+//  as arguments.
+/*
+    The function should return an array containing any combination of elements that add ip to 
+    exactly the targetSum.If there is no combination that adds up to the targetSUm,
+     then return null
+*/
+
+ 

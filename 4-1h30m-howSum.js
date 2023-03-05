d6d43329777170914286fc65fcf26fb133ebc546
Arman-Kabir <armankabir8@gmail.com>
2023-03-05 09:34:26 +0600

    howSum done

--- 4-1h30m-howSum.js
+++ 4-1h30m-howSum.js
@@ -24,9 +24,18 @@ const howSum = (targetSum, numbers) => {
         const remainder = targetSum - num;
         console.log(remainder);
 
-        howSum(remainder,numbers);
+        const remainderResult = howSum(remainder, numbers);
+
+        if (remainderResult !== null) {
+            return [ ...remainderResult, num ];
+        }
     }
+    return null;
 };
 
 console.log(howSum(7, [5, 3, 4, 7]));
+console.log(howSum(7, [2,3]));
+console.log(howSum(7,[2,4]));
+console.log(howSum(8,[2,3,5]));
+// console.log(howSum(300, [7,14]));
 

5670fad532b26f0f0cfb535f9b4fa7f4fd7513a1
Arman-Kabir <armankabir8@gmail.com>
2023-03-18 06:40:02 +0600

    best Sum setup

---
+++ 5-1h52m-bestSum.js
@@ -0,0 +1,22 @@
+/*
+ P.Write a function `bestSum(targetSum,numbers)` that takes in a targetSum and an array of numbers as arguments 
+
+ *The function should return an array containing shortest combination of numbers that add up to exactly the targetSUm.
+
+ *If there is a tie for the shortest combination, u may return any one of the shortest
+
+ bestSum(7,[5,3,4,7]) 
+ bestSum(8,[2,3,5]) 
+
+ */
+
+const bestSum = (targetSum, numbers) => {
+
+}
+
+
+console.log(bestSum(8, [2, 3, 5]));
+console.log(bestSum(7, [5, 3, 4, 7]));
+console.log(bestSum(8, [1, 4, 5]));
+console.log(bestSum(100, [1, 2, 5, 25]));
+

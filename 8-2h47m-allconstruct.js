2840f53cabc23fe3909e8de211411462d4de2682
Arman-Kabir <armankabir8@gmail.com>
2023-03-21 09:33:22 +0600

    allConstruct done

--- 8-2h47m-allconstruct.js
+++ 8-2h47m-allconstruct.js
@@ -20,20 +20,42 @@
     // allConstruct('',[cat,dog,mouse])
 
 */
+//non-optimized
+// const allConstruct = (target, wordBank) => {
+//     if (target === '') return [[]];
+//     const result = [];
+//     for (let word of wordBank) {
+//         if (target.indexOf(word) === 0) {
+//             const suffix = target.slice(word.length);
+//             const suffixWays = allConstruct(suffix, wordBank);
+//             const targetWays = suffixWays.map(way => [word, ...way]);
+//             result.push(...targetWays);
+//         }
+//     }
+//     return result;
+// };
 
-const allConstruct = (target, wordBank) => {
+// optimized
+const allConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
     if (target === '') return [[]];
-
+    const result = [];
     for (let word of wordBank) {
         if (target.indexOf(word) === 0) {
             const suffix = target.slice(word.length);
-            const suffixWays = allConstruct(suffix, wordBank);
-            const targetWays = suffixWays.map(way => [word, ...way])
+            const suffixWays = allConstruct(suffix, wordBank, memo);
+            const targetWays = suffixWays.map(way => [word, ...way]);
+            result.push(...targetWays);
         }
     }
-
+    memo[target] = result;
+    return result;
 };
 
+/**
+ * m= target.length,  n = wordBank.length.....
+ * O(n^m) time ----- O(m) space.
+ */
 
 console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
 console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));

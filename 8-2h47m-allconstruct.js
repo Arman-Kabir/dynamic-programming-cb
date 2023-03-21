08e78f17cbfd02763e92efdccfc6e91f6d121b28
Arman-Kabir <armankabir8@gmail.com>
2023-03-21 07:05:06 +0600

    allConstruct doing

---
+++ 8-2h47m-allconstruct.js
@@ -0,0 +1,32 @@
+/*
+    *Write a function `allConstruct(target,wordBank)` that accepts a target string and an array of strings.
+
+    *The function should return a 2D array containing all of the ways that the `target` can be constructed by concatenating elements of the `wordBank` array.
+
+    Each element of the 2D array should represent one combination that constructs the `target`.
+
+    You may reuse elements of `wordBank` as many times as needed.
+
+    allConstruct(abcdef,[ab,abc,cd,def,abcd]) 
+
+    allConstruct(skateboard,[bo,rd,ate,t,ska,sk,boar]) 
+
+    allConstruct('',[cat,dog,mouse]) 
+
+    allConstruct('purple',[purp,p,ur,le,purpl])
+*/
+
+
+
+console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
+console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
+console.log(allConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
+console.log(allConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
+console.log(allConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeef', [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+]));

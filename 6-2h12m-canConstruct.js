cffc63afc68184e2b25f8699db15017caf095247
Arman-Kabir <armankabir8@gmail.com>
2023-03-21 06:10:16 +0600

    canConstruct done

--- 6-2h12m-canConstruct.js
+++ 6-2h12m-canConstruct.js
@@ -12,7 +12,37 @@
     canConstruct('',[cat,dog,mouse]) -> true
 */
 
-const canConstruct = (target, wordBank) => {
+// non-memoized
+// const canConstruct = (target, wordBank) => {
+//     if (target === '') {
+//         return true;
+//     }
+
+//     for (let word of wordBank) {
+//         console.log(word);
+
+//         if (target.indexOf(word) === 0) {
+//             console.log(target.indexOf(word), target, word);
+
+//             const suffix = target.slice(word.length);
+//             console.log(suffix);
+
+//             if (canConstruct(suffix, wordBank) === true) {
+//                 return true;
+//             };
+//         }
+//     }
+//     return false;
+// };
+
+/**
+ * O(n^m*m) time
+ * O(m^2) space
+ */
+
+// memoized
+const canConstruct = (target, wordBank, memo = {}) => {
+    if (target in memo) return memo[target];
     if (target === '') {
         return true;
     }
@@ -26,22 +56,28 @@ const canConstruct = (target, wordBank) => {
             const suffix = target.slice(word.length);
             console.log(suffix);
 
-            if (canConstruct(suffix, wordBank) === true) {
+            if (canConstruct(suffix, wordBank, memo) === true) {
+                memo[target] = true;
                 return true;
             };
         }
     }
+    memo[target] = false;
     return false;
 };
+/**memoized
+ * O(n*m^2) time
+ * O(m^2 space)
+ */
 
 console.log(canConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
 console.log(canConstruct('skateboard', ['bo', 'rd', 'ate', 't', 'ska', 'sk', 'boar']));
 console.log(canConstruct('enterapotentpot', ['a', 'p', 'ent', 'enter', 'ot', 'o', 't']));
-// console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeef', [
-//     "e",
-//     "ee",
-//     "eee",
-//     "eeee",
-//     "eeeee",
-//     "eeeeee",
-// ]));
+console.log(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeef', [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+])); 

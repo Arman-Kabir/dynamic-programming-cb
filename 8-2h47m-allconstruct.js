6c48915ace149b4bbdc3f64841697f1604ec5518
Arman-Kabir <armankabir8@gmail.com>
2023-03-21 08:37:48 +0600

    allConstruct doing2

--- 8-2h47m-allconstruct.js
+++ 8-2h47m-allconstruct.js
@@ -19,6 +19,21 @@
     
     // allConstruct('',[cat,dog,mouse])
 
+*/
+
+const allConstruct = (target, wordBank) => {
+    if (target === '') return [[]];
+
+    for (let word of wordBank) {
+        if (target.indexOf(word) === 0) {
+            const suffix = target.slice(word.length);
+            const suffixWays = allConstruct(suffix, wordBank);
+            const targetWays = suffixWays.map(way => [word, ...way])
+        }
+    }
+
+};
+
 
 console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
 console.log(allConstruct('abcdef', ['ab', 'abc', 'cd', 'def', 'abcd']));
